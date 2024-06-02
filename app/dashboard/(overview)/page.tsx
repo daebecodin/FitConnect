4990b827afe5ca3adeb85f099e898a7d4537dfe6
daebecodin <duranddyer123@gmail.com>
2024-06-02 10:18:01 -0700

    adding search function to the invoices page. The pagination might still be buggy but i will try to fix

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import Cards from '@/app/ui/dashboard/cards';
 import {poppins} from "@/app/ui/fonts";
 //fertching revenue data for chart
-import {fetchLatestInvoices, } from "@/app/lib/data";
+import {fetchCardData,} from "@/app/lib/data";
 import { Suspense } from "react";
 import CardWrapper from '@/app/ui/dashboard/cards';
 
@@ -12,8 +12,14 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton,CardsSkeleton} from "@/app
 
 export default async function Page() {
 
-    const latestInvoices = await fetchLatestInvoices();
+    // const latestInvoices = await fetchLatestInvoices();
 
+    // const {
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    //     numberOfInvoices,
+    //     numberOfCustomers
+    // } = await fetchCardData()
 
 
     return (
@@ -21,15 +27,21 @@ export default async function Page() {
             <h1 className={`${poppins.className}antialiased mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton />}>
-                    <LatestInvoices />
+            {/*     <Card title="Collected" value={totalPaidInvoices} type="collected" />*/}
+            {/*     <Card title="Pending" value={totalPendingInvoices} type="pending" />*/}
+            {/*     <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />*/}
+            {/*    { <Card title="Total Customers" value={numberOfCustomers} type="customers"/> }*/}
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart/>
                 </Suspense>
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices/>
                 </Suspense>
             </div>
         </main>
